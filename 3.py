7a17d2a422c23f54a25059a66da8266f8a80f00d
shilpa2020 <51980846+shilpa2020@users.noreply.github.com>
2019-06-27 10:20:05 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+ch=input()
+if ch in('a','e','i','o','u'):
+  print("Vowel")
+elif ch in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+  print("Consonant")
+else:
+  print("invalid")
+  
