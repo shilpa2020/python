b9461c29e8e129606b2d5521e077198ac3175ef8
shilpa2020 <51980846+shilpa2020@users.noreply.github.com>
2019-07-07 13:15:22 +0530

    Update 71ab.py

--- 71ab.py
+++ 71ab.py
@@ -1,12 +1,7 @@
-y = input()
-v = 'aeiou'
-for n in v:
-    if n in y:
-        y = ''
-        break
-    else:
-        continue
-if y == '':
-    print('yes')
+l=input()
+for i in l:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u'):
+       print("yes")
+       break
 else:
-    print('no')
+  print("no")
