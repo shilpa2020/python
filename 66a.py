1d4780e21d6ea0e0225455ada9207d07c1dd1f33
shilpa2020 <51980846+shilpa2020@users.noreply.github.com>
2019-07-08 13:59:45 +0530

    Update 66a.py

--- 66a.py
+++ 66a.py
@@ -1,9 +1,9 @@
-a1=int(input())
-r=0
-for n in range(2,a1):
- if(a1%n==0):
-  r=r+1
-if(r<=0):
+b1=int(input())
+e=0
+for n in range(2,b1):
+ if(b1%n==0):
+  e=e+1
+if(e<=0):
  print("yes")
 else:
  print("no")
